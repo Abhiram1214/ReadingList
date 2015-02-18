2323495d73fa86f8bfcb1c8f55155e42f570a4ed
Abhiram <abhiram@abhiram-X550LC>
2015-02-18 15:45:03 +0530

    search added

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,6 +5,18 @@ class Book < ActiveRecord::Base
 
   #"recent" scope implementation is...
 
+  scope :search,->(keyword){where(title: keyword) if keyword.present?}
+
+  #The difference btw scope and class method is that scope always returns a collection.Scope will not return 'nil'.
+
+  #def self.search(keyword)
+   # if keyword.present?
+    #  where(title: keyword)
+    #else
+     # all
+    #end
+ # end
+
   def self.recent
     where('finished_on > ?',2.days.ago)
   end
